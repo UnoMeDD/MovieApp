4f94d73d480c6e6707dd178ba9ff2e211be683f1
UnoMe_DD <DominicDuda@gmx.at>
2022-04-04 18:29:27 +0200

    Deleted Imports and added life cycles

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,28 +1,44 @@
 package com.example.movieapp
 
-import androidx.compose.ui.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.movieapp.ui.theme.MovieAppTheme
 
 
 class MainActivity : ComponentActivity() {
+    override fun onStart(){
+        super.onStart()
+        Log.d("OnStartTest", "I am on start")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d("OnResumeTest", "I am on Resume")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d("OnPauseTest", "I am on Pause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d("onStopTest", "I am on Stop")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d("onDestroyTest", "I am on Destroy")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
